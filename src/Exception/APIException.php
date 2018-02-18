72f9af6db3db2628f326c3dbf9976000c7e5f8fc
Paul Dixon <paul@liblynx.com>
2018-02-18 22:24:38 +0000

    Add custom exception classes and marker interface

---
+++ src/Exception/APIException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LibLynx\Connect\Exception;
+
+/**
+ * Class RuntimeException represents a runtime problem communicating with the LibLynx API
+ *
+ * @package LibLynx\Connect\Exception
+ */
+class APIException extends \RuntimeException implements LibLynxException
+{
+}
---
+++ src/Exception/LibLynxException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LibLynx\Connect\Exception;
+
+/**
+ * This is just a 'marker interface' that all exceptions thrown by the LibLynx Connect client will
+ * implement, allowing client code to easily catch them all
+ * @package LibLynx\Connect\Exception
+ */
+interface LibLynxException
+{
+}
